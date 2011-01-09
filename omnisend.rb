36f012129216ee66957f5ed3e0eb66074ef9a0f3
theirix <theirix@gmail.com>
2011-01-10 00:00:34 +0100

    Send all command line parameters

--- omnisend.rb
+++ omnisend.rb
@@ -1,8 +1,10 @@
 require 'amqp'
 require 'mq'
 
-exit(1) unless ARGV.size == 1
-data = Marshal.dump(ARGV[0])
+exit(1) if ARGV.empty? 
+message = ARGV.join(' ')
+puts "Sending message #{message}"
+data = Marshal.dump(message)
 
 Signal.trap('INT') { AMQP.stop{ EM.stop } }
 
